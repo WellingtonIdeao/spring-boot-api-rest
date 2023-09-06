daafa64e6a22bd2a52cbaf083409f132f71ff4bb
Wellington Ideão <wellington.ideao@gmail.com>
2023-09-06 00:48:22 -0300

    Requisição HTTP POST

---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,13 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(
+        String nome,
+        String email,
+        String telefone,
+        String crm,
+        Especialidade especialidade,
+        DadosEndereco endereco
+) {
+}
